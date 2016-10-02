c640ac8d4822b39464cd946f15ac94b1282a6ee9
divdraw <bander.lviv@gmail.com>
2016-10-02 22:27:09 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,30 +34,75 @@ function selectPan() {
 	if (pan === toolbar.pans.round) {
 		pan.style.backgroundImage = "url(../paint/img/icons/round_pan_focus.png)";
 		pan.style.backgroundPosition = "center";
-		canvas.pan = "rd";
+		toolbar.activePan = "rd";
 	} else if (pan === toolbar.pans.square) {
 		pan.style.backgroundImage = "url(../paint/img/icons/square_pan_focus.png)";
 		pan.style.backgroundPosition = "center";
-		canvas.pan = "sq";
+		toolbar.activePan = "sq";
 	} else if (pan === toolbar.pans.clear) {
 		pan.style.backgroundImage = "url(../paint/img/icons/delete_pan_focus.png)";
 		pan.style.backgroundPosition = "center";
-		canvas.pan = "clear";
+		toolbar.activePan = "clear";
 		canvas.paper.clear();
 	}
 }
 
-function controller() {
-	if (event.which === 1 && canvas.pan !== "clear" && canvas.pan) {
-		var point = document.createElement("div");
-		point.style.top = canvas.cursor.y + "px";
-		point.style.left = canvas.cursor.x + "px";
-		point.setAttribute("class", canvas.pan);
-		canvas.paper.elem.insertAdjacentElement("afterBegin", point);
-	} else {
-		return;
+function selectPanColor() {
+	var color = event.target;
+	for (var i = 0; i < color.parentElement.children.length; i++) {
+		color.parentElement.children[i].style.borderRadius = "";
+		color.parentElement.children[i].style.boxShadow = "";
 	}
+	color.style.borderRadius = "50%";
+	color.style.boxShadow = "0px 0px 6px 4px #949494";
+	if (color === toolbar.color.blue) {
+		toolbar.color.activeColor = "#37abc8";
+	}
+	if (color === toolbar.color.red) {
+		toolbar.color.activeColor = "#ff5555";
+	}
+	if (color === toolbar.color.green) {
+		toolbar.color.activeColor = "#55ff99";
+	}
+}
 
+function getCursor() {
+	if (toolbar.activePan === "rd" && toolbar.color.activeColor === "#37abc8") {
+		canvas.paper.elem.style.cursor = "url(../paint/img/icons/round_pan_focus.png), pointer";
+	}
+	if (toolbar.activePan === "sq" && toolbar.color.activeColor === "#37abc8") {
+		canvas.paper.elem.style.cursor = "url(../paint/img/icons/square_pan_focus.png), pointer";
+	}
+	if (toolbar.activePan === "rd" && toolbar.color.activeColor === "#ff5555") {
+		canvas.paper.elem.style.cursor = "url(../paint/img/icons/color_rad.png), pointer";
+	}
+	if (toolbar.activePan === "sq" && toolbar.color.activeColor === "#ff5555") {
+		canvas.paper.elem.style.cursor = "url(../paint/img/icons/square_red.png), pointer";
+	}
+	if (toolbar.activePan === "rd" && toolbar.color.activeColor === "#55ff99") {
+		canvas.paper.elem.style.cursor = "url(../paint/img/icons/color_green.png), pointer";
+	}
+	if (toolbar.activePan === "sq" && toolbar.color.activeColor === "#55ff99") {
+		canvas.paper.elem.style.cursor = "url(../paint/img/icons/square_green.png), pointer";
+	}
+}
+
+function controller() {
+	getCursor();
+	if (event.which === 1 && toolbar.activePan === "rd") {
+		canvas.paper.elem.insertAdjacentHTML("beforeEnd", "<circle cx=" + (canvas.cursor.x + "") + "  cy=" + (canvas.cursor.y + "") + "  r=\"12.5\" fill=" + toolbar.color.activeColor + "></circle>");
+	} else if (event.which === 1 && toolbar.activePan === "sq") {
+		canvas.paper.elem.insertAdjacentHTML("beforeEnd", "<rect x=" + (canvas.cursor.x + "") + " y=" + (canvas.cursor.y + "") + " width=\"25\" height=\"25\" fill=" + toolbar.color.activeColor + "></rect>");
+	}
+	return;
+}
+
+function defaulrToolbarProperty() {
+	toolbar.pans.round.style.backgroundImage = "url(../paint/img/icons/round_pan_focus.png)";
+	toolbar.pans.round.style.backgroundPosition = "center";
+	toolbar.color.blue.style.borderRadius = "50%";
+	toolbar.color.blue.style.boxShadow = "0px 0px 6px 4px #949494";
+	canvas.paper.elem.style.cursor = "url(../paint/img/icons/round_pan_focus.png), pointer";
 }
 
 var canvas = {
@@ -80,16 +125,14 @@ var canvas = {
 		clear: function() {
 			var workArea = this.elem.parentElement;
 			workArea.removeChild(this.elem);
-			var newPaper = document.createElement("div");
-			newPaper.setAttribute("id", "canvas");
-			newPaper.setAttribute("class", "paint__canvas");
-			workArea.insertAdjacentElement("beforeEnd", newPaper);
-			this.elem = newPaper;
+			// workArea.insertAdjacentHTML("beforeEnd", "<svg id='canvas' class='paint__canvas'></svg>");
+			workArea.insertAdjacentHTML("beforeEnd", "<svg id=\"canvas\" class=\"paint__canvas\"></svg>");
+			this.elem = document.getElementById("canvas");
+			console.log(this.elem);
 			canvas.listener();
 		}
 	},
 	cursor: {},
-	pan: false,
 	listener: function() {
 		this.paper.elem.addEventListener("click", getCoord);
 		this.paper.elem.addEventListener("click", controller);
@@ -114,7 +157,29 @@ var toolbar = {
 		clear: function() {
 			return getElem("rst");
 		}(),
+	},
+	colorPanel: function() {
+		return getElem("colors");
+	}(),
+	activePan: "rd",
+	color: {
+		blue: function() {
+			return getElem("colorBlue");
+		}(),
+		red: function() {
+			return getElem("colorRed");
+		}(),
+		green: function() {
+			return getElem("colorGreen");
+		}(),
+		activeColor: "#37abc8",
+	},
+	listener: function() {
+		toolbar.elem.addEventListener("click", selectPan);
+		toolbar.colorPanel.addEventListener("click", selectPanColor);
 	}
 }
+
+defaulrToolbarProperty();
 canvas.listener();
-toolbar.elem.addEventListener("click", selectPan);
+toolbar.listener();
